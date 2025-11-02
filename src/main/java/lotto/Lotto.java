3432856e03f0f5c531428e1cce44ad2c7f72398f
Hwang Soojin <soojin9433@naver.com>
2025-11-02 18:45:12 +0900

    refactor(lotto): validate 메서드 내에서 로또 번호 범위 검증 메서드 호출

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,6 +14,8 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+
+        checkNumberDomain(numbers);
     }
 
     // TODO: 추가 기능 구현
