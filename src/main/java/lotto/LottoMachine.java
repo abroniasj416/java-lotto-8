d0cb3d3287d96659e9c54fc234cbfb36c9e928e7
Hwang Soojin <soojin9433@naver.com>
2025-11-03 06:52:40 +0900

    feat(domain): 구입 금액 기반 로또 발행 수 계산 기능 구현

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,8 +1,22 @@
 package lotto;
 
 public class LottoMachine {
+    // TODO : 구입 금액만큼 로또 발행 수 계산
+    private int money;
+    private int lottoCount;
+
+    public LottoMachine(int money) {
+        validate(money);
+        this.money = money;
+        this.lottoCount = calculateLottoCount(this.money);
+    }
     public static void validate(int money) {
         if (money % 1000 != 0)
             throw new IllegalArgumentException("[ERROR] 로또 1장의 가격은 1,000원입니다.");
     }
+
+    private int calculateLottoCount(int money) {
+        return money / 1000;
+    }
 }
+
