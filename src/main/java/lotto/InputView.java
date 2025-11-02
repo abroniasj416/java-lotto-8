e4fdc878b6174ead03d5f2ea4249b810db7bd904
Hwang Soojin <soojin9433@naver.com>
2025-11-03 00:36:14 +0900

    refactor(view): 변수명 coin을 money로 변경하여 의미 명확화

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -5,9 +5,9 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     public int readLottoMoney() {
         System.out.println("구입금액을 입력해 주세요.");
-        int coin = Integer.parseInt(Console.readLine());
+        int mony = Integer.parseInt(Console.readLine());
         // TODO : 예외처리) coin 값이 1,000원으로 나누어 떨어지는가?
         // 예를 들어 LottoMachine.validate(coin);
-        return coin;
+        return money;
     }
 }
