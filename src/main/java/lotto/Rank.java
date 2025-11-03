b2ef57b4ba191d0ac8779625c96807457c153766
Hwang Soojin <soojin9433@naver.com>
2025-11-03 16:59:25 +0900

    feat(domain): Rank 생성자 및 getter 메서드 추가

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,7 +1,7 @@
 package lotto;
 
 public enum Rank {
-    FRIST(6, false, 2000000000L),
+    FIRST(6, false, 2000000000L),
     SECOND(5, true, 30000000L),
     THIRD(5, false, 1500000L),
     FOURTH(4, false, 50000L),
@@ -11,4 +11,24 @@ public enum Rank {
     private final int matchCount;
     private final boolean bonusRequired;
     private final long prize;
+
+    Rank(int matchCount, boolean bonusRequired, long prize) {
+        this.matchCount = matchCount;
+        this.bonusRequired = bonusRequired;
+        this.prize = prize;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public boolean isBonusRequired() {
+        return bonusRequired;
+    }
+
+    public long getPrize() {
+        return prize;
+    }
+
+
 }
