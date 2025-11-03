568dff10d414c1133dcc3cc2ce04b25e863bf18f
Hwang Soojin <soojin9433@naver.com>
2025-11-03 16:45:31 +0900

    feat(domain): Rank enum 기본 구조 정의
    - 당첨 등수 상수(FIRST~NONE) 추가
    - 각 등수별 매칭 개수, 보너스 필요 여부, 상금 필드 정의

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,4 +1,14 @@
 package lotto;
 
 public enum Rank {
+    FRIST(6, false, 2000000000L),
+    SECOND(5, true, 30000000L),
+    THIRD(5, false, 1500000L),
+    FOURTH(4, false, 50000L),
+    FIFTH(3, false, 5000L),
+    NONE(0, false, 0L);
+
+    private final int matchCount;
+    private final boolean bonusRequired;
+    private final long prize;
 }
